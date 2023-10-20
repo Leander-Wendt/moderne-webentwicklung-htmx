54c11722b67acbfba5f4bfa1e1dd3f535c9184ba
Leander Wendt <m1573ry@gmail.com>
2023-10-20 16:30:25 +0200

    Added getters & setters

--- forum-react/backend/backend/src/main/java/com/bachelorreact/backend/comment/Comment.java
+++ forum-react/backend/backend/src/main/java/com/bachelorreact/backend/comment/Comment.java
@@ -2,11 +2,9 @@ package com.bachelorreact.backend.comment;
 
 import com.bachelorreact.backend.post.Post;
 import com.bachelorreact.backend.user.User;
-import jakarta.persistence.Entity;
-import jakarta.persistence.GeneratedValue;
-import jakarta.persistence.GenerationType;
-import jakarta.persistence.Id;
+import jakarta.persistence.*;
 
+import java.util.Date;
 import java.util.UUID;
 
 @Entity
@@ -17,7 +15,79 @@ public class Comment {
     private UUID id;
     private String title;
     private String body;
+    private Date created_at;
+    private Date updated_at;
+    @ManyToOne
     private User author;
+    @ManyToOne
     private Post post;
-    
+
+    public Comment(UUID id, String title, String body, Date created_at, Date updated_at, User author, Post post) {
+        this.id = id;
+        this.title = title;
+        this.body = body;
+        this.created_at = created_at;
+        this.updated_at = updated_at;
+        this.author = author;
+        this.post = post;
+    }
+
+    public Comment() {
+    }
+
+    public UUID getId() {
+        return id;
+    }
+
+    public void setId(UUID id) {
+        this.id = id;
+    }
+
+    public String getTitle() {
+        return title;
+    }
+
+    public void setTitle(String title) {
+        this.title = title;
+    }
+
+    public String getBody() {
+        return body;
+    }
+
+    public void setBody(String body) {
+        this.body = body;
+    }
+
+    public Date getCreated_at() {
+        return created_at;
+    }
+
+    public void setCreated_at(Date created_at) {
+        this.created_at = created_at;
+    }
+
+    public Date getUpdated_at() {
+        return updated_at;
+    }
+
+    public void setUpdated_at(Date updated_at) {
+        this.updated_at = updated_at;
+    }
+
+    public User getAuthor() {
+        return author;
+    }
+
+    public void setAuthor(User author) {
+        this.author = author;
+    }
+
+    public Post getPost() {
+        return post;
+    }
+
+    public void setPost(Post post) {
+        this.post = post;
+    }
 }
