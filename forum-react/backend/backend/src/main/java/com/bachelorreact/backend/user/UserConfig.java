bc8a7827c9369d4f3a65da2893cc841fcde471fa
Leander Wendt <m1573ry@gmail.com>
2023-10-22 22:57:23 +0200

    Added passwordEncoder to mockdata

--- forum-react/backend/backend/src/main/java/com/bachelorreact/backend/user/UserConfig.java
+++ forum-react/backend/backend/src/main/java/com/bachelorreact/backend/user/UserConfig.java
@@ -3,17 +3,25 @@ package com.bachelorreact.backend.user;
 import org.springframework.boot.CommandLineRunner;
 import org.springframework.context.annotation.Bean;
 import org.springframework.context.annotation.Configuration;
+import org.springframework.security.crypto.password.PasswordEncoder;
 
 import java.util.List;
 
 @Configuration
 public class UserConfig {
+
+    private final PasswordEncoder passwordEncoder;
+
+    public UserConfig(PasswordEncoder passwordEncoder) {
+        this.passwordEncoder = passwordEncoder;
+    }
+
     @Bean
     CommandLineRunner commandLineRunner(UserRepository repository) {
         return args -> {
-            User userA = new User("Bernd", "Bernd123", UserRole.USER, "Bernd");
-            User userB = new User("JD", "JD123", UserRole.USER, "JD");
-            User userC = new User("Admin", "12345678", UserRole.ADMIN, "Admin");
+            User userA = new User("Bernd", passwordEncoder.encode("Bernd123"), UserRole.USER, "Bernd");
+            User userB = new User("JD", passwordEncoder.encode("JD123"), UserRole.USER, "JD");
+            User userC = new User("Admin", passwordEncoder.encode("12345678"), UserRole.ADMIN, "Admin");
 
             repository.saveAll(List.of(userA, userB, userC));
         };
