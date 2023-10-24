ae7bc3c31a14af53af28141df966db924f8b4bd5
Leander Wendt <m1573ry@gmail.com>
2023-10-24 17:32:43 +0200

    Create ApiExceptionHandler.java

---
+++ forum-react/backend/backend/src/main/java/com/bachelorreact/backend/exception/ApiExceptionHandler.java
@@ -0,0 +1,20 @@
+package com.bachelorreact.backend.exception;
+
+import org.springframework.http.HttpStatus;
+import org.springframework.http.ResponseEntity;
+import org.springframework.web.bind.annotation.ControllerAdvice;
+import org.springframework.web.bind.annotation.ExceptionHandler;
+
+import java.time.ZoneId;
+import java.time.ZonedDateTime;
+
+@ControllerAdvice
+public class ApiExceptionHandler {
+    @ExceptionHandler(value = {ApiRequestException.class})
+    public ResponseEntity<Object> handleApiRequestException(ApiRequestException exception) {
+        ApiException apiException = new ApiException(
+                exception.getMessage(), exception, HttpStatus.BAD_REQUEST, ZonedDateTime.now(ZoneId.of("UTC+2"))
+        );
+        return new ResponseEntity<>(apiException, HttpStatus.BAD_REQUEST);
+    }
+}
