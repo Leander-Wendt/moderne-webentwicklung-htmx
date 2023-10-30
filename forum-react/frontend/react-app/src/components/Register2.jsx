c21cb147faad29b85c17763eee778f5913ebfdd3
Leander Wendt <m1573ry@gmail.com>
2023-10-30 12:57:47 +0100

    Added post request

--- forum-react/frontend/react-app/src/components/Register2.jsx
+++ forum-react/frontend/react-app/src/components/Register2.jsx
@@ -1,105 +1,115 @@
 import { useState } from "react";
 
 export const Register2 = () => {
-	const [inputs, setInputs] = useState({});
+  const [inputs, setInputs] = useState({});
 
-	const handleChange = (event) => {
-		const name = event.target.name;
-		const value = event.target.value;
-		setInputs((values) => ({ ...values, [name]: value }));
-	};
+  const handleChange = (event) => {
+    const name = event.target.name;
+    const value = event.target.value;
+    setInputs((values) => ({ ...values, [name]: value }));
+  };
 
-	const handleSubmit = (event) => {
-		event.preventDefault();
-		alert(JSON.stringify(inputs));
-	};
-	// Styling source: https://tailwindui.com/components/application-ui/forms/sign-in-forms
-	return (
-		<>
-			<section className="flex min-h-full flex-col justify-center px-6 py-12 lg:px-8">
-				<div className="sm:mx-auto sm:w-full sm:max-w-sm">
-					<h2 className="mt-10 text-center text-2xl font-bold leading-9 tracking-tight text-gray-900">
-						Register
-					</h2>
-				</div>
-				<div className="mt-10 sm:mx-auto sm:w-full sm:max-w-sm">
-					<form
-						onSubmit={handleSubmit}
-						className="space-y-6"
-						action="#"
-						method="POST"
-					>
-						<div>
-							<label
-								htmlFor="username"
-								className="block text-sm font-medium leading-6 text-gray-900"
-							>
-								Username
-							</label>
-							<div className="mt-2">
-								<input
-									type="text"
-									name="username"
-									id="username"
-									autoComplete="off"
-									value={inputs.username || ""}
-									required
-									onChange={handleChange}
-									className="block w-full rounded-md border-0 py-1.5 text-gray-900 shadow-sm ring-1 ring-inset ring-gray-300 placeholder:text-gray-400 focus:ring-2 focus:ring-inset focus:ring-indigo-600 sm:text-sm sm:leading-6"
-								/>
-							</div>
-						</div>
+  const handleSubmit = (event) => {
+    event.preventDefault();
+    alert(JSON.stringify(inputs));
+    fetch("http://localhost:8080/api/v1/auth/register", {
+      method: "post",
+      headers: {
+        "Content-Type": "application/json",
+        "Access-Control-Allow-Origin": "http://localhost:8080",
+      },
+      body: JSON.stringify(inputs),
+    }).then((res) => {
+      console.log(res.json());
+    });
+  };
+  // Styling source: https://tailwindui.com/components/application-ui/forms/sign-in-forms
+  return (
+    <>
+      <section className="flex min-h-full flex-col justify-center px-6 py-12 lg:px-8">
+        <div className="sm:mx-auto sm:w-full sm:max-w-sm">
+          <h2 className="mt-10 text-center text-2xl font-bold leading-9 tracking-tight text-gray-900">
+            Register
+          </h2>
+        </div>
+        <div className="mt-10 sm:mx-auto sm:w-full sm:max-w-sm">
+          <form
+            onSubmit={handleSubmit}
+            className="space-y-6"
+            action="#"
+            method="POST"
+          >
+            <div>
+              <label
+                htmlFor="username"
+                className="block text-sm font-medium leading-6 text-gray-900"
+              >
+                Username
+              </label>
+              <div className="mt-2">
+                <input
+                  type="text"
+                  name="username"
+                  id="username"
+                  autoComplete="off"
+                  value={inputs.username || ""}
+                  required
+                  onChange={handleChange}
+                  className="block w-full rounded-md border-0 py-1.5 text-gray-900 shadow-sm ring-1 ring-inset ring-gray-300 placeholder:text-gray-400 focus:ring-2 focus:ring-inset focus:ring-indigo-600 sm:text-sm sm:leading-6"
+                />
+              </div>
+            </div>
 
-						<div>
-							<div className="flex items-center justify-between">
-								<label
-									htmlFor="displayname"
-									className="block text-sm font-medium leading-6 text-gray-900"
-								>
-									Displayname
-								</label>
-							</div>
-							<div className="mt-2">
-								<input
-									type="text"
-									name="displayname"
-									autoComplete="off"
-									value={inputs.displayname || ""}
-									required
-									onChange={handleChange}
-									className="block w-full rounded-md border-0 py-1.5 text-gray-900 shadow-sm ring-1 ring-inset ring-gray-300 placeholder:text-gray-400 focus:ring-2 focus:ring-inset focus:ring-indigo-600 sm:text-sm sm:leading-6"
-								/>
-							</div>
-						</div>
-						<div>
-							<label
-								htmlFor="displayname"
-								className="block text-sm font-medium leading-6 text-gray-900"
-							>
-								Password
-							</label>
-							<div className="mt-2">
-								<input
-									type="password"
-									name="password"
-									autoComplete="off"
-									value={inputs.password || ""}
-									required
-									onChange={handleChange}
-									className="block w-full rounded-md border-0 py-1.5 text-gray-900 shadow-sm ring-1 ring-inset ring-gray-300 placeholder:text-gray-400 focus:ring-2 focus:ring-inset focus:ring-indigo-600 sm:text-sm sm:leading-6"
-								/>
-							</div>
-							<div className="mt-2">
-								<input
-									type="submit"
-									value={"Registrieren"}
-									className="flex w-full justify-center rounded-md bg-indigo-600 px-3 py-1.5 text-sm font-semibold leading-6 text-white shadow-sm hover:bg-indigo-500 focus-visible:outline focus-visible:outline-2 focus-visible:outline-offset-2 focus-visible:outline-indigo-600"
-								/>
-							</div>
-						</div>
-					</form>
-				</div>
-			</section>
-		</>
-	);
+            <div>
+              <div className="flex items-center justify-between">
+                <label
+                  htmlFor="displayname"
+                  className="block text-sm font-medium leading-6 text-gray-900"
+                >
+                  Displayname
+                </label>
+              </div>
+              <div className="mt-2">
+                <input
+                  type="text"
+                  name="displayname"
+                  autoComplete="off"
+                  value={inputs.displayname || ""}
+                  required
+                  onChange={handleChange}
+                  className="block w-full rounded-md border-0 py-1.5 text-gray-900 shadow-sm ring-1 ring-inset ring-gray-300 placeholder:text-gray-400 focus:ring-2 focus:ring-inset focus:ring-indigo-600 sm:text-sm sm:leading-6"
+                />
+              </div>
+            </div>
+            <div>
+              <label
+                htmlFor="displayname"
+                className="block text-sm font-medium leading-6 text-gray-900"
+              >
+                Password
+              </label>
+              <div className="mt-2">
+                <input
+                  type="password"
+                  name="password"
+                  autoComplete="off"
+                  value={inputs.password || ""}
+                  required
+                  onChange={handleChange}
+                  className="block w-full rounded-md border-0 py-1.5 text-gray-900 shadow-sm ring-1 ring-inset ring-gray-300 placeholder:text-gray-400 focus:ring-2 focus:ring-inset focus:ring-indigo-600 sm:text-sm sm:leading-6"
+                />
+              </div>
+              <div className="mt-2">
+                <input
+                  type="submit"
+                  value={"Registrieren"}
+                  className="flex w-full justify-center rounded-md bg-indigo-600 px-3 py-1.5 text-sm font-semibold leading-6 text-white shadow-sm hover:bg-indigo-500 focus-visible:outline focus-visible:outline-2 focus-visible:outline-offset-2 focus-visible:outline-indigo-600"
+                />
+              </div>
+            </div>
+          </form>
+        </div>
+      </section>
+    </>
+  );
 };
