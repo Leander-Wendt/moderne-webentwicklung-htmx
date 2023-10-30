a2daa7892e0b625125851450c8ad20237b574a52
Leander Wendt <m1573ry@gmail.com>
2023-10-30 12:58:23 +0100

    Added cors to register endpoint

--- forum-react/backend/backend/src/main/java/com/bachelorreact/backend/auth/AuthenticationController.java
+++ forum-react/backend/backend/src/main/java/com/bachelorreact/backend/auth/AuthenticationController.java
@@ -2,10 +2,7 @@ package com.bachelorreact.backend.auth;
 
 import lombok.RequiredArgsConstructor;
 import org.springframework.http.ResponseEntity;
-import org.springframework.web.bind.annotation.PostMapping;
-import org.springframework.web.bind.annotation.RequestBody;
-import org.springframework.web.bind.annotation.RequestMapping;
-import org.springframework.web.bind.annotation.RestController;
+import org.springframework.web.bind.annotation.*;
 
 @RestController
 @RequestMapping("/api/v1/auth")
@@ -13,9 +10,8 @@ import org.springframework.web.bind.annotation.RestController;
 public class AuthenticationController {
 
     private final AuthenticationService service;
-
+    @CrossOrigin
     @PostMapping("/register")
-
     public ResponseEntity<AuthenticationResponse> register(@RequestBody RegisterRequest request) {
         return ResponseEntity.ok(service.register(request));
     }
