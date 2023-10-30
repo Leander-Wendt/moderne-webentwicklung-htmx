0b628f3ec770c35542b959bb6ebe6c2edae81aab
Leander Wendt <m1573ry@gmail.com>
2023-10-30 15:18:20 +0100

    Added navbar & login route

--- forum-react/frontend/react-app/src/App.jsx
+++ forum-react/frontend/react-app/src/App.jsx
@@ -1,15 +1,21 @@
 import { BrowserRouter, Route, Routes } from "react-router-dom";
 import { Register2 } from "./components/Register2";
+import { Login } from "./components/Login";
+import { Navbar } from "./components/Navbar";
 
 function App() {
-	return (
-		<BrowserRouter>
-			<Routes>
-				<Route path="/" element={<h1>Hello World</h1>} />
-				<Route path="/register" element={<Register2 />} />
-			</Routes>
-		</BrowserRouter>
-	);
+  return (
+    <>
+      <Navbar />
+      <BrowserRouter>
+        <Routes>
+          <Route path="/" element={<h1>Hello World</h1>} />
+          <Route path="/register" element={<Register2 />} />
+          <Route path="/login" element={<Login />} />
+        </Routes>
+      </BrowserRouter>
+    </>
+  );
 }
 
 export default App;
