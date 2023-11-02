8848084844691edf6ec996f4ea973cbe7546a1d6
Leander Wendt <m1573ry@gmail.com>
2023-11-02 17:09:24 +0100

    Added userReducer & devtools config

--- forum-react/frontend/react-app/src/app/store.js
+++ forum-react/frontend/react-app/src/app/store.js
@@ -1,5 +1,12 @@
 import { configureStore } from "@reduxjs/toolkit";
+import counterReducer from "../features/counter/counterSlice";
+import userReducer from "../components/user/userSlice";
 
-export default configureStore({
-  reducer: {},
-});
+export default configureStore(
+  {
+    reducer: {
+      user: userReducer,
+    },
+  },
+  window.__REDUX_DEVTOOLS_EXTENSION__ && window.__REDUX_DEVTOOLS_EXTENSION__()
+);
