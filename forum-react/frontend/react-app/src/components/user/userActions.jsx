eeba29fb616a8478d93fd1ba6a0e44e5917d3bdb
Leander Wendt <m1573ry@gmail.com>
2023-11-02 17:08:03 +0100

    Create userActions.jsx

---
+++ forum-react/frontend/react-app/src/components/user/userActions.jsx
@@ -0,0 +1,63 @@
+import { createAsyncThunk } from "@reduxjs/toolkit";
+
+const backendURL = "http://127.0.0.1:8080";
+
+export const registerUser = createAsyncThunk(
+  "auth/register",
+  async ({ username, displayname, password }, { rejectWithValue }) => {
+    try {
+      let data = await fetch(`${backendURL}/api/v1/auth/register`, {
+        method: "post",
+        headers: {
+          "Content-Type": "application/json",
+          credentials: "include",
+        },
+        body: JSON.stringify({ username, displayname, password }),
+      })
+        .then((res) => res.json())
+        .then((body) => {
+          localStorage.setItem("jwt", body.token);
+          return body;
+        });
+      return data;
+    } catch (error) {
+      // return custom error message from backend if present
+      if (error.response && error.response.data.message) {
+        return rejectWithValue(error.response.data.message);
+      } else {
+        return rejectWithValue(error.message);
+      }
+    }
+  }
+);
+
+export const loginUser = createAsyncThunk(
+  "auth/login",
+  async ({ username, password }, { rejectWithValue }) => {
+    try {
+      const config = {
+        headers: {
+          "Content-Type": "application/json",
+          credentials: "include",
+        },
+      };
+      let data = await fetch(
+        `${backendURL}/api/v1/auth/login`,
+        { method: "post", config },
+        { username, password }
+      )
+        .then((res) => res.json())
+        .then((body) => {
+          localStorage.setItem("jwt", body.token);
+        });
+      return data;
+    } catch (error) {
+      // return custom error message from backend if present
+      if (error.response && error.response.data.message) {
+        return rejectWithValue(error.response.data.message);
+      } else {
+        return rejectWithValue(error.message);
+      }
+    }
+  }
+);
