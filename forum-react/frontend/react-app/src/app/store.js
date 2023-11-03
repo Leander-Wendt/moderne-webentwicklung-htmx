6ca2742dcd718c927362018bbd4746939720ff96
Leander Wendt <m1573ry@gmail.com>
2023-11-03 13:32:27 +0100

    Added redux-persist

--- forum-react/frontend/react-app/src/app/store.js
+++ forum-react/frontend/react-app/src/app/store.js
@@ -1,11 +1,26 @@
 import { configureStore } from "@reduxjs/toolkit";
 import userReducer from "../components/user/userSlice";
+import { persistStore, persistReducer } from "redux-persist";
+import storage from "redux-persist/lib/storage";
 
-export default configureStore(
+const persistConfig = {
+  key: "root",
+  storage,
+};
+
+const persistedReducer = persistReducer(persistConfig, userReducer);
+
+export const store = configureStore(
   {
     reducer: {
-      user: userReducer,
+      user: persistedReducer,
     },
+    middleware: (getDefaultMiddleware) =>
+      getDefaultMiddleware({
+        serializableCheck: false,
+      }),
   },
   window.__REDUX_DEVTOOLS_EXTENSION__ && window.__REDUX_DEVTOOLS_EXTENSION__()
 );
+
+export const persistor = persistStore(store);
