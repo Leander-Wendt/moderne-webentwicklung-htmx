fa4480c7853f3ee24199b53cb7466e094ee16dce
Leander Wendt <m1573ry@gmail.com>
2023-11-03 16:15:34 +0100

    Fixed login endpoint

--- forum-react/backend/backend/src/main/java/com/bachelorreact/backend/auth/AuthenticationController.java
+++ forum-react/backend/backend/src/main/java/com/bachelorreact/backend/auth/AuthenticationController.java
@@ -7,16 +7,17 @@ import org.springframework.web.bind.annotation.*;
 @RestController
 @RequestMapping("/api/v1/auth")
 @RequiredArgsConstructor
+@CrossOrigin
 public class AuthenticationController {
 
     private final AuthenticationService service;
-    @CrossOrigin
+
     @PostMapping("/register")
     public ResponseEntity<AuthenticationResponse> register(@RequestBody RegisterRequest request) {
         return ResponseEntity.ok(service.register(request));
     }
 
-    @PostMapping("/authenticate")
+    @PostMapping("/login")
     public ResponseEntity<AuthenticationResponse> register(@RequestBody AuthenticationRequest request) {
         return ResponseEntity.ok(service.authenticate(request));
     }
