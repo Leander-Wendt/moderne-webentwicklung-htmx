4df5b02cf3dab8ffe725b544cd51442fad61793a
Leander Wendt <m1573ry@gmail.com>
2023-11-07 18:10:07 +0100

    Refactored store.js path

--- forum-react/frontend/react-app/src/main.jsx
+++ forum-react/frontend/react-app/src/main.jsx
@@ -3,7 +3,7 @@ import React from "react";
 import ReactDOM from "react-dom/client";
 import App from "./App.jsx";
 import { Provider } from "react-redux";
-import { store, persistor } from "./app/store";
+import { store, persistor } from "./store/store";
 import { PersistGate } from "redux-persist/integration/react";
 
 ReactDOM.createRoot(document.getElementById("root")).render(
