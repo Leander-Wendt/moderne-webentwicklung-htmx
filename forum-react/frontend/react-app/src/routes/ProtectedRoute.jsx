5863be5adafcb775f68b563f8e5934c4bde1311c
Leander Wendt <m1573ry@gmail.com>
2023-11-07 13:45:03 +0100

    Create ProtectedRoute.jsx

---
+++ forum-react/frontend/react-app/src/routes/ProtectedRoute.jsx
@@ -0,0 +1,21 @@
+// ProtectedRoute.js
+import { useSelector } from "react-redux";
+import { NavLink, Outlet } from "react-router-dom";
+
+const ProtectedRoute = () => {
+  const { userToken } = useSelector((state) => state.user);
+
+  if (!userToken) {
+    return (
+      <div className="unauthorized">
+        <h1>Unauthorized :</h1>
+        <span>
+          <NavLink to="/login">Login</NavLink> to gain access
+        </span>
+      </div>
+    );
+  }
+
+  return <Outlet />;
+};
+export default ProtectedRoute;
