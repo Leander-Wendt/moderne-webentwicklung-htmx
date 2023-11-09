d2e58d0723c64954ed26869b2696ad51bb8fd5fa
Leander Wendt <m1573ry@gmail.com>
2023-11-09 11:12:19 +0100

    Disabling Cors

--- forum-react/backend/backend/src/main/java/com/bachelorreact/backend/config/SecurityConfiguration.java
+++ forum-react/backend/backend/src/main/java/com/bachelorreact/backend/config/SecurityConfiguration.java
@@ -26,6 +26,7 @@ public class SecurityConfiguration {
     public SecurityFilterChain securityFilterChain(HttpSecurity http) throws Exception {
         return http
                 .csrf(csrf -> csrf.disable())
+                .cors(cors -> cors.disable())
                 .authorizeHttpRequests(auth -> auth
                         .requestMatchers("/api/v1/auth/**").permitAll()
                         .requestMatchers("/posts").permitAll()
