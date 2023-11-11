e156d26aa77dbb70e27c64d0169e242eb02073a1
Leander Wendt <m1573ry@gmail.com>
2023-11-11 18:45:17 +0100

    Added TODO

--- forum-react/frontend/react-app/src/App.jsx
+++ forum-react/frontend/react-app/src/App.jsx
@@ -7,26 +7,26 @@ import { Post } from "./components/Post";
 import { PageError } from "./components/PageError";
 import { ProtectedRoute } from "./routes/ProtectedRoute";
 import { CreatePost } from "./components/CreatePost";
-// TODO:
+// TODO: Fix Create Post Cors Error, probably issues with req body
 // Crud Post
 function App() {
-  return (
-    <>
-      <BrowserRouter>
-        <Navbar />
-        <Routes>
-          <Route path="/" element={<Posts />} />
-          <Route path="/register" element={<Register />} />
-          <Route path="/login" element={<Login />} />
-          <Route element={<ProtectedRoute />}>
-            <Route path="/post/new" element={<CreatePost />} />
-          </Route>
-          <Route path="/post/:id" element={<Post />} />
-          <Route path="*" element={<PageError />} />
-        </Routes>
-      </BrowserRouter>
-    </>
-  );
+	return (
+		<>
+			<BrowserRouter>
+				<Navbar />
+				<Routes>
+					<Route path="/" element={<Posts />} />
+					<Route path="/register" element={<Register />} />
+					<Route path="/login" element={<Login />} />
+					<Route element={<ProtectedRoute />}>
+						<Route path="/post/new" element={<CreatePost />} />
+					</Route>
+					<Route path="/post/:id" element={<Post />} />
+					<Route path="*" element={<PageError />} />
+				</Routes>
+			</BrowserRouter>
+		</>
+	);
 }
 
 export default App;
