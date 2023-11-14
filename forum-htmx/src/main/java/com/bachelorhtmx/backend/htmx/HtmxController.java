5ffc60001ada7bd5f94d9224f330804307f0cc9c
Leander Wendt <m1573ry@gmail.com>
2023-11-14 19:08:06 +0100

    Setup controller

--- forum-htmx/src/main/java/com/bachelorhtmx/backend/htmx/HtmxController.java
+++ forum-htmx/src/main/java/com/bachelorhtmx/backend/htmx/HtmxController.java
@@ -1,16 +1,13 @@
 package com.bachelorhtmx.backend.htmx;
 
 import org.springframework.stereotype.Controller;
-import org.springframework.ui.Model;
-import org.springframework.web.bind.annotation.RequestMapping;
-import org.springframework.web.bind.annotation.RequestMethod;
+import org.springframework.web.bind.annotation.*;
 
 @Controller
 public class HtmxController {
 
-    @RequestMapping(path = "/", method = RequestMethod.GET)
-    public String index(Model model) {
-        // addAttributesForIndex(model, ListFilter.ALL);
+    @GetMapping("/")
+    public String index() {
         return "index";
     }
 }
