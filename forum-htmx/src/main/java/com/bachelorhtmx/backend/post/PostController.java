af381ed71821f080e3628d2a99fc46b599b73404
Leander Wendt <m1573ry@gmail.com>
2023-11-14 18:12:39 +0100

    Update PostController.java

--- forum-htmx/src/main/java/com/bachelorhtmx/backend/post/PostController.java
+++ forum-htmx/src/main/java/com/bachelorhtmx/backend/post/PostController.java
@@ -1,6 +1,7 @@
 package com.bachelorhtmx.backend.post;
 
 import org.springframework.http.HttpHeaders;
+import org.springframework.ui.Model;
 import org.springframework.web.bind.annotation.*;
 
 import java.util.List;
