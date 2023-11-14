f56af97f8cfd30bc819136d77a85a77facfeffc2
Leander Wendt <m1573ry@gmail.com>
2023-11-14 18:12:22 +0100

    Create HtmxController.java

---
+++ forum-htmx/src/main/java/com/bachelorhtmx/backend/htmx/HtmxController.java
@@ -0,0 +1,16 @@
+package com.bachelorhtmx.backend.htmx;
+
+import org.springframework.stereotype.Controller;
+import org.springframework.ui.Model;
+import org.springframework.web.bind.annotation.RequestMapping;
+import org.springframework.web.bind.annotation.RequestMethod;
+
+@Controller
+public class HtmxController {
+
+    @RequestMapping(path = "/", method = RequestMethod.GET)
+    public String index(Model model) {
+        // addAttributesForIndex(model, ListFilter.ALL);
+        return "index";
+    }
+}
