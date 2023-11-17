e8c24c1564e2f85120477d72f1c71752fd26ae55
Leander Wendt <m1573ry@gmail.com>
2023-11-17 19:57:21 +0100

    Update HtmxController.java

--- forum-htmx/src/main/java/com/bachelorhtmx/backend/htmx/HtmxController.java
+++ forum-htmx/src/main/java/com/bachelorhtmx/backend/htmx/HtmxController.java
@@ -1,14 +1,23 @@
 package com.bachelorhtmx.backend.htmx;
 
+import com.bachelorhtmx.backend.post.PostService;
 import org.springframework.stereotype.Controller;
+import org.springframework.ui.Model;
 import org.springframework.web.bind.annotation.*;
 
 @Controller
 public class HtmxController {
 
+    private final PostService postService;
+
+    public HtmxController(PostService postService) {
+        this.postService = postService;
+    }
+
     @GetMapping("/")
-    public String index() {
-        return "index";
+    public String index(Model model) {
+        model.addAttribute("posts", postService.getPosts());
+        return "index.html";
     }
 
     @GetMapping("/login")
