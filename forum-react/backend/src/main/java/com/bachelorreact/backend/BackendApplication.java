73b52ed95da868245938f465f2f643ead12a4d2b
Leander Wendt <m1573ry@gmail.com>
2023-12-31 00:35:17 +0100

    Now changes DB URL depending on context

--- forum-react/backend/src/main/java/com/bachelorreact/backend/BackendApplication.java
+++ forum-react/backend/src/main/java/com/bachelorreact/backend/BackendApplication.java
@@ -3,9 +3,16 @@ package com.bachelorreact.backend;
 import org.springframework.boot.SpringApplication;
 import org.springframework.boot.autoconfigure.SpringBootApplication;
 
+import java.util.Properties;
+
 @SpringBootApplication
 public class BackendApplication {
     public static void main(String[] args) {
+        if (System.getenv("DB_URL") == null) {
+            Properties appProps = new Properties();
+            appProps.setProperty("spring.datasource.url", "jdbc:postgresql://localhost:5432/postgres");
+        }
+
         SpringApplication.run(BackendApplication.class, args);
     }
 }
