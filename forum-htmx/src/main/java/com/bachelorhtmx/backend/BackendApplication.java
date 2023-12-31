db3d54962b6d94b6fbca14f4bd21e332dba11799
Leander Wendt <m1573ry@gmail.com>
2023-12-31 17:36:22 +0100

    Removed inefficient logic

--- forum-htmx/src/main/java/com/bachelorhtmx/backend/BackendApplication.java
+++ forum-htmx/src/main/java/com/bachelorhtmx/backend/BackendApplication.java
@@ -4,16 +4,9 @@ import org.springframework.boot.SpringApplication;
 import org.springframework.boot.autoconfigure.SpringBootApplication;
 import org.springframework.boot.autoconfigure.security.servlet.SecurityAutoConfiguration;
 
-import java.util.Properties;
-
 @SpringBootApplication(exclude = {SecurityAutoConfiguration.class })
 public class BackendApplication {
     public static void main(String[] args) {
-        if (System.getenv("DB_URL") != null) {
-            Properties appProps = new Properties();
-            appProps.setProperty("spring.datasource.url", System.getenv("DB_URL"));
-        }
-
         SpringApplication.run(BackendApplication.class, args);
     }
 }
