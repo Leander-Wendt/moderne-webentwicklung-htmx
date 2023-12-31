828111a30c682a5ed8e6d73206faa9e456d58e32
Leander Wendt <m1573ry@gmail.com>
2023-12-31 15:36:14 +0100

    Added host url and port

--- forum-react/frontend/vite.config.js
+++ forum-react/frontend/vite.config.js
@@ -3,5 +3,16 @@ import react from "@vitejs/plugin-react";
 
 // https://vitejs.dev/config/
 export default defineConfig({
+	base: "/",
 	plugins: [react()],
+	server: {
+		port: 5173,
+		strictPort: true,
+		host: "0.0.0.0",
+		origin: "http://0.0.0.0:5173",
+	},
+	preview: {
+		port: 5173,
+		strictPort: true,
+	},
 });
